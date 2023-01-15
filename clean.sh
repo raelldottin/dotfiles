65f4b1fae6d57f0ddef0e47f4261531785542a87
Raell Dottin <raell.dottin@proton.me>
2023-01-15 16:46:01 -0500

    add script for uninstallation

---
+++ clean.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+runclean() {
+  local_file="$1"
+  if rm -f "$local_file"; then
+    echo "Successfully deleted $local_file"
+  fi
+}
+	echo "Uninstalling configuration files:"
+	runclean "$HOME/.config/nvim/init.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/core/colorscheme.lua"
+	runclean "$HOME/.config/nvim/lua/r2e/core/keymaps.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/core/options.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/autopairs.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/comment.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/gitsigns.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/lsp/lspconfig.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/lsp/mason.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/lsp/null-ls.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/lualine.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/mason.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/nvim-cmp.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/nvim-tree.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/tabnine.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/telescope.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins/treesitter.lua"
+  runclean "$HOME/.config/nvim/lua/r2e/plugins-setup.lua"
+  runclean "$HOME/.hyper.js"
+  runclean "$HOME/.tmux.conf"
+  runclean "$HOME/.zshrc"
+	if [[ -d ~/.local ]]; then
+		echo "If you encounter issues, please delete the respective files in $HOME/.local"
+	fi;
+
+	echo "Uninstallation complete."
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 <!---
 This file is auto-generate by a github hook please modify readme.template if you don't want to loose your work
 -->
-# raelldottin/dotfiles v0.0.1-21
+# raelldottin/dotfiles v0.0.1-22
 
 **Table of Contents**
 
