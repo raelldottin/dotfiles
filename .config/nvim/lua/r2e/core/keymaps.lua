e4412a321856a43637d0d772e18cf6bbfdf0b44e
Raell Dottin <raell.dottin@proton.me>
2023-01-15 13:59:09 -0500

    add lpsaga keymaps

--- .config/nvim/lua/r2e/core/keymaps.lua
+++ .config/nvim/lua/r2e/core/keymaps.lua
@@ -48,16 +48,64 @@ keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string u
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 
--- telescope git commands (not on youtube nvim video)
+-- telescope git commands
 keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
 keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
 keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
 keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
 
--- restart lsp server (not on youtube nvim video)
+-- restart lsp server
 keymap.set("n", "<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
 
 -- debugging keymaps
-vim.keymap.set("n", "<leader>dn", ":lua require'dap-python'.test_method()<CR>")
-vim.keymap.set("n", "<leader>df", ":lua require'dap-python'.test_class()<CR>")
-vim.keymap.set("n", "<leader>dn", ":lua require'dap-python'.test_method()<CR>")
+keymap.set("n", "<leader>dn", ":lua require'dap-python'.test_method()<CR>")
+keymap.set("n", "<leader>df", ":lua require'dap-python'.test_class()<CR>")
+keymap.set("n", "<leader>dn", ":lua require'dap-python'.test_method()<CR>")
+
+--lpsaga keybinds
+-- Lsp finder find the symbol definition implement reference
+-- if there is no implement it will hide
+-- when you use action in finder like open vsplit then you can
+-- use <C-t> to jump back
+keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
+-- Code action
+keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+
+-- Rename
+keymap("n", "gr", "<cmd>Lspsaga rename<CR>")
+-- Peek Definition
+-- you can edit the definition file in this flaotwindow
+-- also support open/vsplit/etc operation check definition_action_keys
+-- support tagstack C-t jump back
+keymap("n", "gd", "<cmd>Lspsaga peek_definition<CR>")
+-- Go to Definition
+keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
+-- Show line diagnostics you can pass arugment ++unfocus to make
+-- show_line_diagnsotic float window unfocus
+keymap("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+-- Show cursor diagnostic
+-- also like show_line_diagnostics  support pass ++unfocus
+keymap("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
+-- Show buffer diagnostic
+keymap("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
+-- Diagnsotic jump can use `<c-o>` to jump back
+keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+-- Diagnostic jump with filter like Only jump to error
+keymap("n", "[E", function()
+  require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end)
+keymap("n", "]E", function()
+  require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
+end)
+-- Toggle Outline
+keymap("n", "<leader>o", "<cmd>Lspsaga outline<CR>")
+-- Hover Doc
+-- if there has no hover will have a notify no information available
+-- to disable it just Lspsaga hover_doc ++quiet
+keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+-- Callhierarchy
+keymap("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+keymap("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+-- Float terminal
+keymap({ "n", "t" }, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@ This file is auto-generate by a github hook please modify readme.template if you
 -->
 # dotfiles
 
-# raelldottin/dotfiles v0.0.1-9
+# raelldottin/dotfiles v0.0.1-10
 
 **Table of Contents**
 
