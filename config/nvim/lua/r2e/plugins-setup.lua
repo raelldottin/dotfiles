bc56fa860b84eb307c842cc396a5fc27ec5002e3
Raell Dottin <raell.dottin@proton.me>
2023-01-16 23:54:47 -0500

    add lspsaga nvim plugin

--- config/nvim/lua/r2e/plugins-setup.lua
+++ config/nvim/lua/r2e/plugins-setup.lua
@@ -81,6 +81,13 @@ return packer.startup(function(use)
   use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
   use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
   use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+  use({
+    "glepnir/lspsaga.nvim",
+    branch = "main",
+    config = function()
+      require("lspsaga").setup({})
+    end,
+  })
 
   -- formatting & linting
   use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
