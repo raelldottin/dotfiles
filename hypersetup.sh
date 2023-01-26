5981111461439971f0c7d7e069ccca225063e462
Raell Dottin <raelldottin@raelldottin.com>
2023-01-26 17:16:08 -0500

    add ostype check for proper processing on ubuntu

--- hypersetup.sh
+++ hypersetup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "Setting up Hyper configuration file."
-ln hyper.js "$HOME"/.hyper.js
-echo "Hyper configuration complete."
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  echo "Hyper is only for macOS"
+  exit 0
+else
+  echo "Setting up Hyper configuration file."
+  ln hyper.js "$HOME"/.hyper.js
+  echo "Hyper configuration complete."
+fi
