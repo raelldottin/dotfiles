2b629e15ebd3cbab9ad2e71412cf3137afc19f2c
Raell Dottin <raelldottin@raelldottin.com>
2023-02-03 10:43:17 -0500

    change url for ohmyzsh repo

--- zshsetup.sh
+++ zshsetup.sh
@@ -3,11 +3,11 @@
 echo "Setting up Zsh configuration file."
 ln zshrc "$HOME"/.zshrc
 if which curl; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 elif which wget; then
-  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 elif which fetch; then
-  sh -c "$(fetch -o - https://raw.githubusercontent.com/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(fetch -o - https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
   echo "No utility found to perform download."
 fi
