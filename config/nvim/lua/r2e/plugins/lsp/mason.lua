07090f0496ada044e6c69e8c9b4ed504d48f90bb
Raell Dottin <raelldottin@raelldottin.com>
2023-02-10 15:28:12 -0500

    add pyright and update formatting

--- config/nvim/lua/r2e/plugins/lsp/mason.lua
+++ config/nvim/lua/r2e/plugins/lsp/mason.lua
@@ -1,69 +1,70 @@
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-  return
+	return
 end
 
 -- import mason-lspconfig plugin safely
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-  return
+	return
 end
 
 -- import mason-null-ls plugin safely
 local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_status then
-  return
+	return
 end
 
 -- enable mason
 mason.setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
-    },
-  },
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 mason_lspconfig.setup({
-  -- list of servers for mason to install
-  ensure_installed = {
-    "bashls",
-    "clangd",
-    "dockerls",
-    "golangci_lint_ls",
-    "gopls",
-    "jsonls",
-    "sumneko_lua",
-    "prosemd_lsp",
-    "marksman",
-    "powershell_es",
-    "terraformls",
-    "tflint",
-  },
-  -- auto-install configured servers (with lspconfig)
-  automatic_installation = true, -- not the same as ensure_installed
+	-- list of servers for mason to install
+	ensure_installed = {
+		"bashls",
+		"clangd",
+		"dockerls",
+		"golangci_lint_ls",
+		"gopls",
+		"jsonls",
+		"sumneko_lua",
+		"prosemd_lsp",
+		"marksman",
+		"powershell_es",
+		"terraformls",
+		"tflint",
+	},
+	-- auto-install configured servers (with lspconfig)
+	automatic_installation = true, -- not the same as ensure_installed
 })
 
 mason_null_ls.setup({
-  -- list of formatters & linters for mason to install
-  ensure_installed = {
-    "cpplint",
-    "clang_format",
-    "hadolint",
-    "gitlint",
-    "gofumpt",
-    "stylua",
-    "alex",
-    "autopep8",
-    "black",
-    "pylint",
-    "shellcheck",
-    "shellharden",
-    "shfmt",
-    "stylua", -- lua formatter
-  },
-  -- auto-install configured formatters & linters (with null-ls)
-  automatic_setup = false,
+	-- list of formatters & linters for mason to install
+	ensure_installed = {
+		"cpplint",
+		"clang_format",
+		"hadolint",
+		"gitlint",
+		"gofumpt",
+		"stylua",
+		"alex",
+		"autopep8",
+		"black",
+		"pyright",
+		"pylint",
+		"shellcheck",
+		"shellharden",
+		"shfmt",
+		"stylua", -- lua formatter
+	},
+	-- auto-install configured formatters & linters (with null-ls)
+	automatic_setup = false,
 })
