740d6b6979dfa957ec6c0d90d3196519d19b0b03
Raell Dottin <raell.dottin@gmail.com>
2023-04-27 17:16:51 -0400

    add dependency script

---
+++ pre-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Automate git configuration
+#~/Documents/Personal/dotfiles dev* ❯ git config --global user.email "raell.dottin@gmail.com"                                                                          05:10:46 PM
+#~/Documents/Personal/dotfiles dev* ❯ git config --global user.name "Raell Dottin"                                                                                     05:11:09 PM
