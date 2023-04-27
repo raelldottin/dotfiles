138f27d673c76b8d004135ab041425385b28286b
Raell Dottin <raell.dottin@gmail.com>
2023-04-27 17:30:47 -0400

    change tpm plugin directory location

--- tmuxsetup.sh
+++ tmuxsetup.sh
@@ -2,27 +2,33 @@
 
 echo "Setting up tmux configuration file."
 case "$OSTYPE" in
-  linux-gnu)
-    TMUX_CONF="linux-gnu_tmux.conf"
-    ;;
-  darwin*)
-    TMUX_CONF="darwin_tmux.conf"
-    ;;
-  *)
-    echo "Unable to determine operating system"
-    exit 1
-    ;;
+linux-gnu)
+	TMUX_CONF="linux-gnu_tmux.conf"
+	;;
+darwin*)
+	TMUX_CONF="darwin_tmux.conf"
+	;;
+*)
+	echo "Unable to determine operating system"
+	exit 1
+	;;
 esac
+
 if [[ ! -f "$HOME"/.tmux.conf ]]; then
-  ln "$TMUX_CONF" "$HOME"/.tmux.conf
+	ln -fn "$TMUX_CONF" "$HOME"/.tmux.conf
+else
+	echo "$HOME/.tmux.conf is already configured."
 fi
+
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
-  mkdir -p ~/.tmux/plugins/tpm
+	mkdir -p ~/.tmux/plugins/tpm
+else
+	echo "$HOME/.tmux/plugins/tpm already exists."
 fi
+
 if [[ -d "$HOME"/.tmux/plugins/tpm ]]; then
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-  echo "Please use prefix + I in a tmux session to install pending plugins."
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins
+	echo "Please use prefix + I in a tmux session to install pending plugins."
 fi
-echo "Tmux configuration complete."
 
+echo "Tmux configuration complete."
