8d76d42d7ff44aeb0637112140a788738c7f00c0
Raell Dottin <raell.dottin@gmail.com>
2023-04-27 17:23:41 -0400

    change file formatting

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,9 +1,16 @@
-require("mason").setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
-    },
-  },
+--import mason plugin safely
+local status, mason = pcall(require, "mason")
+if not status then
+	return
+end
+
+--configure treesitter
+mason.setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
