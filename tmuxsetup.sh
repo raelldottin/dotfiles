39e4cecbb693bcf02b9816084a59f0e420b55d62
Raell Dottin <raell.dottin@gmail.com>
2023-09-30 09:29:21 -0400

    change to add improvements
    Consistent Variable Naming: Introduced meaningful variable names for paths and directories.
    
    Improved Output Messages: Output messages provide more information about what actions are being taken.
    
    Directory Existence Checks: Added checks for the existence of directories before creating or cloning.
    
    Error Handling: The script now exits with an error message and status if the operating system can't be determined.
    
    Variable Expansion: Used variable expansion for cleaner code.

--- tmuxsetup.sh
+++ tmuxsetup.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
 echo "Setting up tmux configuration file."
+
 case "$OSTYPE" in
-linux-gnu)
-	TMUX_CONF="linux-gnu_tmux.conf"
-	;;
-darwin*)
-	TMUX_CONF="darwin_tmux.conf"
-	;;
-*)
-	echo "Unable to determine operating system"
-	exit 1
-	;;
+  linux-gnu)
+    TMUX_CONF="linux-gnu_tmux.conf"
+    ;;
+  darwin*)
+    TMUX_CONF="darwin_tmux.conf"
+    ;;
+  *)
+    echo "Error: Unable to determine operating system"
+    exit 1
+    ;;
 esac
 
-if [[ ! -f "$HOME"/.tmux.conf ]]; then
-	ln -fn "$TMUX_CONF" "$HOME"/.tmux.conf
+TMUX_CONF_PATH="$HOME/.tmux.conf"
+
+if [[ ! -f "$TMUX_CONF_PATH" ]]; then
+  ln -fn "$TMUX_CONF" "$TMUX_CONF_PATH"
+  echo "$TMUX_CONF_PATH linked to $TMUX_CONF"
 else
-	echo "$HOME/.tmux.conf is already configured."
+  echo "$TMUX_CONF_PATH is already configured."
 fi
 
-if [[ ! -d ~/.tmux/plugins ]]; then
-	mkdir -p ~/.tmux/plugins
+TMUX_PLUGINS_DIR="$HOME/.tmux/plugins"
+TPM_DIR="$TMUX_PLUGINS_DIR/tpm"
+
+if [[ ! -d "$TMUX_PLUGINS_DIR" ]]; then
+  mkdir -p "$TMUX_PLUGINS_DIR"
+  echo "Created $TMUX_PLUGINS_DIR directory."
 else
-	echo "$HOME/.tmux/plugins already exists."
+  echo "$TMUX_PLUGINS_DIR already exists."
 fi
 
-if [[ -d "$HOME"/.tmux/plugins/tpm ]]; then
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins
-	echo "Please use prefix + I in a tmux session to install pending plugins."
+if [[ ! -d "$TPM_DIR" ]]; then
+  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+  echo "Tmux Plugin Manager (TPM) installed."
+  echo "Please use prefix + I in a tmux session to install pending plugins."
+else
+  echo "$TPM_DIR already exists. Skipping TPM installation."
 fi
 
 echo "Tmux configuration complete."
