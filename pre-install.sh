0a7bd4a5cf51ca766c72192f35597287e390e3ab
Raell Dottin <raell.dottin@gmail.com>
2023-09-30 09:15:10 -0400

    change for cleaner structure
    Check for Homebrew Installation:
    
    Before running homebrewsetup.sh, check if Homebrew is already installed.
    
    Use a Function for Git Configuration:
    
    Wrap the Git configuration commands in a function for better organization.

--- pre-install.sh
+++ pre-install.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-./homebrewsetup.sh
+# Function to check if Homebrew is installed
+check_homebrew() {
+	if ! command -v brew &>/dev/null; then
+		echo "Homebrew not found. Installing Homebrew..."
+		/bin/bash ./homebrewsetup.sh
+	else
+		echo "Homebrew is already installed."
+	fi
+}
 
-git config --global user.email "raell.dottin@gmail.com"
-git config --global user.name "Raell Dottin"
+# Function for Git configuration
+configure_git() {
+	git config --global user.email "raell.dottin@gmail.com"
+	git config --global user.name "Raell Dottin"
+	echo "Git configuration completed."
+}
+
+# Check and install Homebrew
+check_homebrew
+
+# Run Homebrew setup script
+
+# Configure Git
+configure_git
