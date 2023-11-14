0ca15753ae5315a88d3eb65f3b0154dcb493729d
Raell Dottin <raell.dottin@gmail.com>
2023-11-14 07:33:33 -0500

    Updated tab and indent settings to 4 spaces in Neovim options

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -5,8 +5,8 @@ opt.relativenumber = true -- show relative line numbers
 opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.tabstop = 4 -- 4 spaces for tabs
+opt.shiftwidth = 4 -- 4 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
