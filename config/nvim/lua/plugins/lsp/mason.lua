641b74a94f38c3af4265e9541ecacae8f883522f
Raell Dottin <raell.dottin@gmail.com>
2023-11-18 10:00:44 -0500

    Added ruff_lsp to mason configuration and updated linters/formatters in mason_null_ls

--- config/nvim/lua/plugins/lsp/mason.lua
+++ config/nvim/lua/plugins/lsp/mason.lua
@@ -29,17 +29,17 @@ mason.setup({
 mason_lspconfig.setup({
   -- list of servers for mason to install
   ensure_installed = {
-    "bashls",
+    "bashls", -- bash language server
     "clangd",
     "dockerls",
     "golangci_lint_ls",
-    "gopls",
     "jsonls",
     "lua_ls",
     "prosemd_lsp",
     "marksman",
     "terraformls",
     "tflint",
+    "ruff_lsp",
   },
   -- auto-install configured servers (with lspconfig)
   automatic_installation = true, -- not the same as ensure_installed
@@ -55,8 +55,7 @@ mason_null_ls.setup({
     "gofumpt",
     "stylua",
     "alex",
-    "black",
-    "pyright",
+    "ruff",
     "shellcheck",
     "shfmt",
     "stylua", -- lua formatter
